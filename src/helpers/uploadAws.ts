6c0bcb70b53efb171ff2a792b41acfb923a614ad
wrz <fw51640@gmail.com>
2021-05-13 11:02:16 +0800

    fix(aws): set ContentType on upload
    fix #9

--- src/helpers/uploadAws.ts
+++ src/helpers/uploadAws.ts
@@ -1,13 +1,14 @@
 
 import AWS from 'aws-sdk'
-import {S3, BUCKET} from './awss3'
+import { S3, BUCKET } from './awss3'
 
 
-export default async function uploadAws (fileName: string, fileBuffer: Buffer) {
+export default async function uploadAws(fileName: string, fileBuffer: Buffer) {
     const params: AWS.S3.PutObjectRequest = {
         Bucket: BUCKET,
         Key: fileName,
         Body: fileBuffer,
+        ContentType: 'image/jpeg',
     }
 
     S3.upload(params, (error, data) => {
